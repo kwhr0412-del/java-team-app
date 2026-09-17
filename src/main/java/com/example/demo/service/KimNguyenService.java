fb2ea3b798eb7f09996f516cfe035a2b92f189af
3030920 <3030920@30309-20>
2026-09-17 14:34:04 +0900

    自己紹介を修正しました。

--- src/main/java/com/example/demo/service/KimNguyenService.java
+++ src/main/java/com/example/demo/service/KimNguyenService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class KimNguyenService {
 
 	public String getProfile() {
-		return "メンバーのKim Nguyenです。趣味は旅行です。";
+		return "Kim Nguyenです。趣味は旅行です。";
 	}
 }
 
