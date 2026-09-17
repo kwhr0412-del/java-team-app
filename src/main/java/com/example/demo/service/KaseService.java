c4262ab1255746abca63bc272f8851dcab31baf3
3030918 <3030918@30309-18>
2026-09-17 14:26:47 +0900

    自己紹介変更

--- src/main/java/com/example/demo/service/KaseService.java
+++ src/main/java/com/example/demo/service/KaseService.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class KaseService {
     public String getProfile() {
-        return "メンバーの田中です！趣味は趣味はドライブです。";
+        return "メンバー加瀬です！趣味は趣味はドライブです。";
     }
 }
