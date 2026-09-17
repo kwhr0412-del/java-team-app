99c53f3f0aae6638425cbd16acecd846838c97fa
3030920 <3030920@30309-20>
2026-09-17 14:25:00 +0900

    自己紹介を追加(Kim Nguyen)

---
+++ src/main/java/com/example/demo/service/KimNguyenService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KimNguyenService {
+
+	public String getProfile() {
+		return "メンバーのKim Nguyenです！趣味は旅行です。";
+	}
+}
+
